480335dbb01da6ddc1d7b86039ea7f896e328f79
Rodion Basmanov <st135699@student.spbu.ru>
2024-10-07 23:17:54 +0300

    Opening a file

--- Assignment2/assignment2a.cpp
+++ Assignment2/assignment2a.cpp
@@ -6,4 +6,12 @@ int main()
 {
     std::string inputFileName = "source.pdf";
     std::streampos fileSize = std::filesystem::file_size(inputFileName); 
+
+    std::ifstream openFile;
+    openFile.open(inputFileName, std::ios::binary | std::ios::in);
+    if (!openFile.is_open())
+    {
+        std::cout << "Error file" << std::endl;
+    }
+
 }
