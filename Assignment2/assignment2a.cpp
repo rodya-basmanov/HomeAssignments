a46649306639fbd1559a6c1c84fa851725c64b6e
Rodion Basmanov <st135699@student.spbu.ru>
2024-10-08 00:06:41 +0300

    Expanded the array, opened a new file for writing, wrote the array to a new file, freed up memory

--- Assignment2/assignment2a.cpp
+++ Assignment2/assignment2a.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <filesystem>
+#include <algorithm>
 
 int main()
 {
@@ -14,4 +15,20 @@ int main()
         std::cout << "Error file" << std::endl;
     }
 
+    char* buffer = new char[fileSize];
+
+    openFile.read(buffer, fileSize);
+    openFile.close();
+
+    for (std::streampos i = 0; i < fileSize / 2; ++i) 
+    { 
+        std::swap(buffer[i], buffer[fileSize - 1 - i]);
+    }
+    std::ofstream outputFile;
+    std::string outputFileName = "output.pdf";
+    outputFile.open(outputFileName, std::ios::binary | std::ios::out);
+    outputFile.write(buffer, fileSize);
+    outfile.close();
+    delete[] buffer;
+    return 0;
 }
