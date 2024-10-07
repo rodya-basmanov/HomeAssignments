7f01f8e3850a55e38c46520c63121c5b920e956d
Rodion Basmanov <st135699@student.spbu.ru>
2024-10-08 00:12:41 +0300

    added a title

--- Assignment2/assignment2a.cpp
+++ Assignment2/assignment2a.cpp
@@ -1,3 +1,12 @@
+/*# Assignment 2
+## Author
+Basmanov Rodion Gennadievich B81-mm
+## Contacts
+st135699@student.spbu.ru
+## Description
+Home assingment 2a*/
+
+
 #include <iostream>
 #include <fstream>
 #include <filesystem>
