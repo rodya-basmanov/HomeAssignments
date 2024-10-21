8f01d5df15b8ca4990ba9c14c59a2c6bded92342
Rodion Basmanov <st135699@student.spbu.ru>
2024-10-22 00:06:16 +0300

    add autobot.cpp file

---
+++ Assignment3/autobot.cpp
@@ -0,0 +1,19 @@
+#include "autobot.h"
+#include "iostream"
+
+Autobot::Autobot(int strength, int speed, int armor) : Transformer(strength, speed, armor)
+{
+    this->isLeader = isLeader;
+}
+bool Autobot::getIsLeader()
+{
+    return isLeader;
+}
+void Autobot::setIsLeader(bool isLeader)
+{
+    this->isLeader = isLeader; 
+}
+void Autobot::specialMove()
+{
+    std::cout << "Autobot performs a special move\n";
+}
