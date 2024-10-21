14f95872dc4bd8709e85535dd239e2040b366431
Rodion Basmanov <st135699@student.spbu.ru>
2024-10-21 23:52:14 +0300

    add autobot.h file

---
+++ Assignment3/autobot.h
@@ -0,0 +1,17 @@
+#ifndef AUTOBOT_H
+#define AUTOBOT_H
+#include "transformer.h"
+
+class Autobot : public Transformer
+{
+private:
+    bool isLeader;
+public:
+    Autobot(int strength, int speed, int armor);
+
+    bool getIsLeader();
+    void setIsLeader(bool isLeader);
+
+    void specialMove();
+};
+#endif
