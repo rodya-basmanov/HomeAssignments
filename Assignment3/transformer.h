37aaa0aabe26c5f817b7d4d3af07d5304279883d
Rodion Basmanov <st135699@student.spbu.ru>
2024-10-21 23:14:40 +0300

    create transformer.h file

---
+++ Assignment3/transformer.h
@@ -0,0 +1,24 @@
+#ifndef TRANSFORMER_H
+#define TRANSFORMET_H
+
+class Transformer
+{
+private:
+    int strength;
+    int speed;
+    int armor;
+public:
+    Transformer(int strength, int speed, int armor);
+    int getStrength();
+    void setStrength(int strength);
+
+    int getSpeed();
+    void setSpeed(int speed);
+
+    int getArmor();
+    void setArmor(int armor);
+
+    void transform();
+
+};
+#endif
