fee3a67c1c8c6c580fd5ab03e0cd9be45cb77775
Rodion Basmanov <st135699@student.spbu.ru>
2024-10-24 21:12:26 +0300

    edit main.cpp file

--- Assignment3/main.cpp
+++ Assignment3/main.cpp
@@ -1,5 +1,6 @@
-#include "transformer.h"
 #include "autobot.h"
+#include "dinobot.h"
+#include "decepticon.h"
 #include <iostream>
 
 int main()
@@ -7,5 +8,15 @@ int main()
     Autobot optimus(100, 20, 100, true);
     optimus.transform();
     optimus.specialMove();
+    return 0;
+
+    Decepticon megatron(120, 40, 100, "Blaster");
+    megatron.transform();
+    megatron.attack();
+
+    Dinobot grimlock(150, 70, 100, "Dino mode");
+    grimlock.transform();
+    grimlock.roar();
+    
     return 0;
 }
