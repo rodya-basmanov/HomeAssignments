e4859d8aa21a1b4aef0388c035c05df29ef421d4
Rodion Basmanov <st135699@student.spbu.ru>
2024-11-02 23:59:58 +0300

    adding an overload of the comparison operator and output

--- Assignment4/dinobot.cpp
+++ Assignment4/dinobot.cpp
@@ -6,9 +6,32 @@ st135699@student.spbu.ru
 ## Description
 assignment 4 */
 
-#include "dinobot.h"
+#include "decepticon.h"
 
-bool MiniRobot::transformToVehicle() {
-    std::cout << "Transforming to dinobot!" << std::endl;
+Decepticon::Decepticon() : health(120), armor(60), firepower(85), currentWeapon(12) {}
+Decepticon::Decepticon(unsigned int h, unsigned int a, unsigned int f, unsigned int w)
+    : health(h), armor(a), firepower(f), currentWeapon(w) {}
+
+bool Decepticon::transformToVehicle()
+{
+    std::cout << "Decepticon transforming to vehicle!" << std::endl;
     return true;
+}
+
+bool Decepticon::operator>(const Decepticon& other) const
+{
+    return firepower > other.firepower;
+}
+bool Decepticon::operator<(const Decepticon& other) const
+{
+    return firepower < other.firepower;
+}
+
+std::ostream& operator<<(std::ostream& os, const Decepticon& decepticon)
+{
+    os << "Decepticon - Health: " << decepticon.health
+       << ", Armor: " << decepticon.armor
+       << ", Firepower: " << decepticon.firepower
+       << ", Weapon: " << decepticon.currentWeapon;
+    return os;
 }
