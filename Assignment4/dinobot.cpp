b74d50ff1c14ad85dc7222e952bd7fc987add28e
Rodion Basmanov <st135699@student.spbu.ru>
2024-11-03 01:05:34 +0300

    edit file

--- Assignment4/dinobot.cpp
+++ Assignment4/dinobot.cpp
@@ -6,32 +6,32 @@ st135699@student.spbu.ru
 ## Description
 assignment 4 */
 
-#include "decepticon.h"
+#include "dinobot.h"
 
-Decepticon::Decepticon() : health(120), armor(60), firepower(85), currentWeapon(12) {}
-Decepticon::Decepticon(unsigned int h, unsigned int a, unsigned int f, unsigned int w)
+Dinobot::Dinobot() : health(130), armor(70), firepower(90), currentWeapon(8) {}
+Dinobot::Dinobot(unsigned int h, unsigned int a, unsigned int f, unsigned int w)
     : health(h), armor(a), firepower(f), currentWeapon(w) {}
 
-bool Decepticon::transformToVehicle()
+bool Dinobot::transformToVehicle()
 {
-    std::cout << "Decepticon transforming to vehicle!" << std::endl;
+    std::cout << "Dinobot transforming to dinosaur mode!" << std::endl;
     return true;
 }
 
-bool Decepticon::operator>(const Decepticon& other) const
+bool Dinobot::operator>(const Dinobot& other) const
 {
     return firepower > other.firepower;
 }
-bool Decepticon::operator<(const Decepticon& other) const
+bool Dinobot::operator<(const Dinobot& other) const
 {
     return firepower < other.firepower;
 }
 
-std::ostream& operator<<(std::ostream& os, const Decepticon& decepticon)
+std::ostream& operator<<(std::ostream& os, const Dinobot& dinobot)
 {
-    os << "Decepticon - Health: " << decepticon.health
-       << ", Armor: " << decepticon.armor
-       << ", Firepower: " << decepticon.firepower
-       << ", Weapon: " << decepticon.currentWeapon;
+    os << "Dinobot - Health: " << dinobot.health
+       << ", Armor: " << dinobot.armor
+       << ", Firepower: " << dinobot.firepower
+       << ", Weapon: " << dinobot.currentWeapon;
     return os;
 }
