a3925dfbf7d12132f90491f4f36c17a0660adeea
Rodion Basmanov <st135699@student.spbu.ru>
2024-11-02 23:34:17 +0300

    adding an overload of the comparison operator and output

--- Assignment4/autobot.cpp
+++ Assignment4/autobot.cpp
@@ -8,7 +8,30 @@ assignment 4 */
 
 #include "autobot.h"
 
-bool Autobot::transformToVehicle() {
-    std::cout << "Transforming to vehicle!" << std::endl;
+Autobot::Autobot() : health(100), armor(50), firepower(75), currentWeapon(10) {}
+Autobot::Autobot(unsigned int h, unsigned int a, unsigned int f, unsigned int w)
+    : health(h), armor(a), firepower(f), currentWeapon(w) {}
+
+bool Autobot::transformToVehicle()
+{
+    std::cout << "Autobot transforming to vehicle!" << std::endl;
     return true;
+}
+
+bool Autobot::operator>(const Autobot& other) const
+{
+    return firepower > other.firepower;
+}
+bool Autobot::operator<(const Autobot& other) const
+{
+    return firepower < other.firepower;
+}
+
+std::ostream& operator<<(std::ostream& os, const Autobot& autobot)
+{
+    os << "Autobot - Health: " << autobot.health
+       << ", Armor: " << autobot.armor
+       << ", Firepower: " << autobot.firepower
+       << ", Weapon: " << autobot.currentWeapon;
+    return os;
 }
