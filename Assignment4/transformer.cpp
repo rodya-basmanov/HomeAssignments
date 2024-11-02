f77adfaca04d6068961d2651bef81623867c5cd2
Rodion Basmanov <st135699@student.spbu.ru>
2024-11-03 00:07:10 +0300

    adding an overload of the comparison operator and output

--- Assignment4/transformer.cpp
+++ Assignment4/transformer.cpp
@@ -7,19 +7,35 @@ st135699@student.spbu.ru
 assignment 4 */
 
 #include "transformer.h"
+#include <iostream>
 
 TransformerBase::TransformerBase() : health(100), armor(50), currentWeapon(10), firepower(75) {}
 
-bool TransformerBase::move() {
+bool TransformerBase::move()
+{
     return true;
 }
-bool TransformerBase::jump() {
+
+bool TransformerBase::jump()
+{
     return true;
 }
-bool TransformerBase::attack() {
-    if (currentWeapon > 0) {
+
+bool TransformerBase::attack()
+{
+    if (currentWeapon > 0)
+    {
         currentWeapon--;
         return true;
     }
     return false;
+}
+
+std::ostream& operator<<(std::ostream& os, const TransformerBase& transformer)
+{
+    os << "Transformer - Health: " << transformer.health
+       << ", Armor: " << transformer.armor
+       << ", Firepower: " << transformer.firepower
+       << ", Weapon: " << transformer.currentWeapon;
+    return os;
 }
