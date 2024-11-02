6f68d189fa67c04469d933a9a7be79cab60e0fd9
Rodion Basmanov <st135699@student.spbu.ru>
2024-11-03 01:05:57 +0300

    add more tests

--- Assignment4/test.cpp
+++ Assignment4/test.cpp
@@ -7,27 +7,80 @@ st135699@student.spbu.ru
 assignment 4 */
 
 #include <gtest/gtest.h>
-#include "transformer.h"
+#include "autobot.h"
+#include "decepticon.h"
+#include "dinobot.h"
+#include <sstream>
 
-TEST(TransformerTest, AttackDecreasesWeapon) {
-    TransformerBase transformer;
-    unsigned int initialWeapon = transformer.getWeapon();
-    transformer.attack();
-    EXPECT_EQ(transformer.getWeapon(), initialWeapon - 1);
+TEST(AutobotTest, ConstructorAndAttributes) {
+    Autobot autobot(100, 50, 80, 10);
+    EXPECT_EQ(autobot.getWeapon(), 10);
 }
 
-TEST(TransformerTest, JumpSuccessful) {
+TEST(AutobotTest, OutputOperator) {
+    Autobot autobot(100, 50, 80, 10);
+    std::ostringstream os;
+    os << autobot;
+    EXPECT_NE(os.str().find("Autobot - Health: 100"), std::string::npos);
+}
+
+TEST(AutobotTest, ComparisonOperator) {
+    Autobot autobot1(100, 50, 80, 10);
+    Autobot autobot2(100, 50, 75, 10);
+    EXPECT_TRUE(autobot1 > autobot2);
+    EXPECT_FALSE(autobot1 < autobot2);
+}
+
+TEST(DecepticonTest, ConstructorAndAttributes) {
+    Decepticon decepticon(120, 60, 85, 12);
+    EXPECT_EQ(decepticon.getWeapon(), 12);
+}
+
+TEST(DecepticonTest, OutputOperator) {
+    Decepticon decepticon(120, 60, 85, 12);
+    std::ostringstream os;
+    os << decepticon;
+    EXPECT_NE(os.str().find("Decepticon - Health: 120"), std::string::npos);
+}
+
+TEST(DecepticonTest, ComparisonOperator) {
+    Decepticon decepticon1(120, 60, 85, 12);
+    Decepticon decepticon2(120, 60, 75, 10);
+    EXPECT_TRUE(decepticon1 > decepticon2);
+    EXPECT_FALSE(decepticon1 < decepticon2);
+}
+
+TEST(DinobotTest, ConstructorAndAttributes) {
+    Dinobot dinobot(130, 70, 90, 8);
+    EXPECT_EQ(dinobot.getWeapon(), 8);
+}
+
+TEST(DinobotTest, OutputOperator) {
+    Dinobot dinobot(130, 70, 90, 8);
+    std::ostringstream os;
+    os << dinobot;
+    EXPECT_NE(os.str().find("Dinobot - Health: 130"), std::string::npos);
+}
+
+TEST(DinobotTest, ComparisonOperator) {
+    Dinobot dinobot1(130, 70, 90, 8);
+    Dinobot dinobot2(130, 70, 80, 6);
+    EXPECT_TRUE(dinobot1 > dinobot2);
+    EXPECT_FALSE(dinobot1 < dinobot2);
+}
+
+TEST(TransformerBaseTest, BasicFunctions) {
     TransformerBase transformer;
+    transformer.setWeapon(10);
+    EXPECT_EQ(transformer.getWeapon(), 10);
+
+    transformer.attack();
+    EXPECT_EQ(transformer.getWeapon(), 9);
+    EXPECT_TRUE(transformer.move());
     EXPECT_TRUE(transformer.jump());
 }
 
-TEST(TransformerTest, MoveSuccessful) {
-    TransformerBase transformer;
-    EXPECT_TRUE(transformer.move());
-}
-
-TEST(TransformerTest, SetAndGetWeapon) {
-    TransformerBase transformer;
-    transformer.setWeapon(15);
-    EXPECT_EQ(transformer.getWeapon(), 15);
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
