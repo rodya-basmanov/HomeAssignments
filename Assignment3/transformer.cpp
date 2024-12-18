9a7272a4bae055a2fa3a28f03d839bfbffa117b1
Rodion Basmanov <st135699@student.spbu.ru>
2024-12-18 21:43:02 +0300

    Added get-set methods, object fields (Weapon and Armor), and a virtual destructor.

--- Assignment3/transformer.cpp
+++ Assignment3/transformer.cpp
@@ -8,14 +8,20 @@ assignment 3 */
 
 #include "transformer.h"
 
-TransformerBase::TransformerBase() : health(100), armor(50), currentWeapon(10), firepower(75) {}
+TransformerBase::TransformerBase()
+    : health(100), armor(50), currentWeapon(10), firepower(75),
+      primaryWeapon("Blaster", 25), defensiveArmor("Titanium", 40) {}
+
+TransformerBase::~TransformerBase() {}
 
 bool TransformerBase::move() {
     return true;
 }
+
 bool TransformerBase::jump() {
     return true;
 }
+
 bool TransformerBase::attack() {
     if (currentWeapon > 0) {
         currentWeapon--;
