65db21a2ddfb3e2382a78daba96207fae0218202
Rodion Basmanov <st135699@student.spbu.ru>
2024-12-19 02:11:34 +0300

    add reverse herader

---
+++ Assignment2/Assignment2a/reverse.h
@@ -0,0 +1,16 @@
+/*# Assignment 2
+## Author
+Basmanov Rodion Gennadievich B81-mm
+## Contacts
+st135699@student.spbu.ru
+## Description
+Home assingment 2a*/
+
+#ifndef REVERSE_H
+#define REVERSE_H
+
+namespace file_operations {
+  void reverseFileContent();
+}
+
+#endif
