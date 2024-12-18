df8669cb83147b88981c1e7fecc48ce44936b6a0
Rodion Basmanov <st135699@student.spbu.ru>
2024-12-19 01:30:52 +0300

    edit decepticon header file

--- Assignment3/decepticon.h
+++ Assignment3/decepticon.h
@@ -9,12 +9,18 @@ assignment 3 */
 #ifndef DECEPTICON_H
 #define DECEPTICON_H
 
-#include <iostream>
 #include "transformer.h"
 
-class Decepticon : public TransformerBase {
+class decepticon : public transformer {
+private:
+    uint _danger_level;
+
 public:
-    bool transformToVehicle();
+    decepticon(uint health, uint armor, uint weapon, uint power, uint danger_level);
+    uint get_danger_level() const;
+    void set_danger_level(uint danger_level);
+
+    void transform() const override;
 };
 
 #endif
