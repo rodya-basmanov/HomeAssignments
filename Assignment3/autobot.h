035f373ffabcbe2ffd9f2eee80a3202dd29a7f3e
Rodion Basmanov <st135699@student.spbu.ru>
2024-12-19 01:30:33 +0300

    edit autobot header file

--- Assignment3/autobot.h
+++ Assignment3/autobot.h
@@ -6,15 +6,29 @@ st135699@student.spbu.ru
 ## Description
 assignment 3 */
 
-#ifndef AUTOBOTS_H
-#define AUTOBOTS_H
+#ifndef AUTOBOT_H
+#define AUTOBOT_H
 
-#include <iostream>
 #include "transformer.h"
 
-class Autobot : public TransformerBase {
+class autobot : public transformer {
+private:
+    std::string _weapon_type;
+    uint _skill_level;
+    uint _inventory_size;
+
 public:
-    bool transformToVehicle();
+    autobot(uint health, uint armor, uint weapon, uint power, const std::string& weapon_type, uint skill_level = 0, uint inventory_size = 50);
+    std::string get_weapon_type() const;
+    void set_weapon_type(const std::string& weapon_type);
+
+    uint get_skill_level() const;
+    void set_skill_level(uint skill_level);
+
+    uint get_inventory_size() const;
+    void set_inventory_size(uint inventory_size);
+
+    void transform() const override;
 };
 
 #endif
