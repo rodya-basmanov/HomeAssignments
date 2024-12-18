fd869018372f32c5fb57dbd83dfe718f1da29d9d
Rodion Basmanov <st135699@student.spbu.ru>
2024-12-19 02:11:11 +0300

    add main.cpp

---
+++ Assignment2/Assignment2a/main.cpp
@@ -0,0 +1,13 @@
+/*# Assignment 2
+## Author
+Basmanov Rodion Gennadievich B81-mm
+## Contacts
+st135699@student.spbu.ru
+## Description
+Home assingment 2a*/
+
+#include "reverse.h"
+
+int main() {
+    file_operations::reverseFileContent();
+}
