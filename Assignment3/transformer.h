99a896463fb5f4c82901edb9e27fb279aae0f734
Rodion Basmanov <st135699@student.spbu.ru>
2024-12-19 01:31:45 +0300

    edit transformer header file

--- Assignment3/transformer.h
+++ Assignment3/transformer.h
@@ -9,80 +9,38 @@ assignment 3 */
 #ifndef TRANSFORMER_H
 #define TRANSFORMER_H
 
+#include "weapon.h"
+#include "armor.h"
 #include <string>
 
-// Класс для оружия
-class Weapon {
-public:
-    Weapon(const std::string& name = "Default Weapon", unsigned int damage = 0)
-        : name(name), damage(damage) {}
-
-    std::string getName() const { return name; }
-    unsigned int getDamage() const { return damage; }
-
-    void setName(const std::string& newName) { name = newName; }
-    void setDamage(unsigned int newDamage) { damage = newDamage; }
-
-private:
-    std::string name;
-    unsigned int damage;
-};
-
-// Класс для брони
-class Armor {
-public:
-    Armor(const std::string& type = "Default Armor", unsigned int strength = 0)
-        : type(type), strength(strength) {}
-
-    std::string getType() const { return type; }
-    unsigned int getStrength() const { return strength; }
-
-    void setType(const std::string& newType) { type = newType; }
-    void setStrength(unsigned int newStrength) { strength = newStrength; }
-
-private:
-    std::string type;
-    unsigned int strength;
-};
-
-// Базовый класс Transformer
-class TransformerBase {
-public:
-    TransformerBase();
-    virtual ~TransformerBase();
-
-    // Методы движения
-    virtual bool move();
-    virtual bool jump();
-    virtual bool attack();
-
-    // Get-set методы для работы с полями
-    void setHealth(unsigned int h) { health = h; }
-    unsigned int getHealth() const { return health; }
-
-    void setArmor(unsigned int a) { armor = a; }
-    unsigned int getArmor() const { return armor; }
-
-    void setCurrentWeapon(unsigned int w) { currentWeapon = w; }
-    unsigned int getCurrentWeapon() const { return currentWeapon; }
-
-    void setFirepower(unsigned int f) { firepower = f; }
-    unsigned int getFirepower() const { return firepower; }
-
-    const Weapon& getPrimaryWeapon() const { return primaryWeapon; }
-    void setPrimaryWeapon(const Weapon& weapon) { primaryWeapon = weapon; }
-
-    const Armor& getDefensiveArmor() const { return defensiveArmor; }
-    void setDefensiveArmor(const Armor& armor) { defensiveArmor = armor; }
-
+class transformer {
 protected:
-    unsigned int health;         // Здоровье
-    unsigned int armor;          // Уровень брони
-    unsigned int currentWeapon;  // Текущее количество зарядов
-    unsigned int firepower;      // Сила атаки
+    uint _health;
+    uint _armor;
+    uint _weapon;
+    uint _power;
 
-    Weapon primaryWeapon;        // Поле-объект для оружия
-    Armor defensiveArmor;        // Поле-объект для брони
+public:
+    transformer(uint health, uint armor, uint weapon, uint power);
+    virtual ~transformer() = default;
+
+    uint get_health() const;
+    void set_health(uint health);
+
+    uint get_armor() const;
+    void set_armor(uint armor);
+
+    uint get_weapon() const;
+    void set_weapon(uint weapon);
+
+    uint get_power() const;
+    void set_power(uint power);
+
+    virtual void transform() const = 0;
+
+    bool motion() const;
+    bool jump() const;
+    bool attack() const;
 };
 
 #endif
